a9e8f91f416da3f540d84c4efa05d7f1cb01e870
Geovana Augusta <geovanaag@hotmail.com>
2023-02-13 18:07:02 -0300

    feat: praticando getStaticProps - pre-rendering

--- components/Footer.js
+++ components/Footer.js
@@ -2,8 +2,10 @@ import styles from '../styles/Footer.module.css'
 
 export default function Footer() {
   return (
+    <div className={styles.footerContainer}>
     <footer className={styles.footer}>
       <p>Footer &copy;</p>
     </footer>
+    </div>
   )
 }
